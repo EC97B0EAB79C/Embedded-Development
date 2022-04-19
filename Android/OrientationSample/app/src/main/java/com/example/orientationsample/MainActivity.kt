9669cfd5840921ff7d5efa04400df47146da184b
C <99bannyzzz@gmail.com>
2022-04-19 20:12:29 +0900

    Added Comment

--- Android/OrientationSample/app/src/main/java/com/example/orientationsample/MainActivity.kt
+++ Android/OrientationSample/app/src/main/java/com/example/orientationsample/MainActivity.kt
@@ -12,15 +12,20 @@ import android.widget.CheckBox
 import android.widget.TextView
 import kotlin.math.floor
 
-class MainActivity : AppCompatActivity(),SensorEventListener {
-    private lateinit var sensorManager: SensorManager
-    private var mAccelerationValue= FloatArray(3)
-    private var mGeoMagneticValue= FloatArray(3)
-    private var mOrientationValue=  FloatArray(3)
-    private var mInRotationMatrix= FloatArray(9)
-    private var mOutRotationMatrix= FloatArray(9)
-    private var mInclinationMatrix= FloatArray(9)
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    // Sensor Variable
+    private lateinit var sensorManager: SensorManager
+
+    // Orientation variables
+    private var mAccelerationValue = FloatArray(3)
+    private var mGeoMagneticValue = FloatArray(3)
+    private var mOrientationValue = FloatArray(3)
+    private var mInRotationMatrix = FloatArray(9)
+    private var mOutRotationMatrix = FloatArray(9)
+    private var mInclinationMatrix = FloatArray(9)
+
+    // View variables
     private lateinit var mCheckBoxOrientation: CheckBox
     private lateinit var mAzimuthText: TextView
     private lateinit var mRollText: TextView
@@ -30,31 +35,47 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val accelerationSensor: Sensor =sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val magneticSensor:Sensor=sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        sensorManager.registerListener(this,accelerationSensor,SensorManager.SENSOR_DELAY_UI)
-        sensorManager.registerListener(this,magneticSensor,SensorManager.SENSOR_DELAY_UI)
 
+        /*
+        Declare Sensor Manager.
+        Next, get acceleration and magnetic sensor then register listener for both sensors
+         */
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerationSensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val magneticSensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        sensorManager.registerListener(this, accelerationSensor, SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_UI)
+
+
+        /*
+        Assign Views
+        TODO(change code to binding)
+         */
         mAzimuthText = findViewById(R.id.text_view_azimuth)
         mRollText = findViewById(R.id.text_view_roll)
         mPitchText = findViewById(R.id.text_view_pitch)
 
-        val buttonOrientation: Button =findViewById(R.id.button_orientation)
+        val buttonOrientation: Button = findViewById(R.id.button_orientation)
         buttonOrientation.setOnClickListener {
-            if(!mCheckBoxOrientation.isChecked) {
+            if (!mCheckBoxOrientation.isChecked) {
                 updateText()
             }
         }
-        mCheckBoxOrientation=findViewById(R.id.checkbox_orientation)
+        mCheckBoxOrientation = findViewById(R.id.checkbox_orientation)
     }
 
+
+    /*
+    Define action for sensor input
+    When sensor value changes, this function updates values
+    And when real time display is on, call updateText function to update ViewText
+     */
     override fun onSensorChanged(p0: SensorEvent?) {
-        when(p0?.sensor?.type){
-            Sensor.TYPE_ACCELEROMETER -> mAccelerationValue= p0.values
-            Sensor.TYPE_MAGNETIC_FIELD ->mGeoMagneticValue= p0.values
+        when (p0?.sensor?.type) {
+            Sensor.TYPE_ACCELEROMETER -> mAccelerationValue = p0.values
+            Sensor.TYPE_MAGNETIC_FIELD -> mGeoMagneticValue = p0.values
         }
-        if(mCheckBoxOrientation.isChecked){
+        if (mCheckBoxOrientation.isChecked) {
             updateText()
         }
     }
@@ -63,7 +84,11 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         // Do Nothing
     }
 
-    fun updateText(){
+
+    /*
+    Calculates orientation and updates TextView
+     */
+    private fun updateText() {
         SensorManager.getRotationMatrix(
             mInRotationMatrix,
             mInclinationMatrix,
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Embedded Development
+This repository is a collection of codes written for the class "Practical development for Embedded and IoT systems(0ALD502)."
+All Android Project code is written in Kotlin instead of JAVA.
  
