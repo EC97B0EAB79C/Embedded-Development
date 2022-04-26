67aa160a91a3d6fe7920cbe656385d62836b4b76
C <99bannyzzz@gmail.com>
2022-04-26 15:56:46 +0900

    Intent Sample

---
+++ Android/IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.intentsample
+
+import android.content.Intent
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intentsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOrientation.setOnClickListener {
+            val intent: Intent = Intent(this,OrientationActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonLocation.setOnClickListener {
+            val intent: Intent = Intent(this,LocationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
