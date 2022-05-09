653ac2d5069083154c42149119dc4636725ef131
C <99bannyzzz@gmail.com>
2022-05-09 16:51:35 +0900

    Add permission request

--- Android/TheULTRADELUXEAlarm/app/src/main/java/com/tkbaze/theultradeluxealarm/MainActivity.kt
+++ Android/TheULTRADELUXEAlarm/app/src/main/java/com/tkbaze/theultradeluxealarm/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.tkbaze.theultradeluxealarm
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.tkbaze.theultradeluxealarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        // Log Tag
+        private const val TAG="MainActivity"
+        // Permission variables
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,Manifest.permission.ACCESS_FINE_LOCATION
+            ).toTypedArray()
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    // Check if all permission is granted
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /*
+    Runs after permission request
+    runs camera if permission is granted
+    closes app when permission is not granted
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                TODO("function after checking permission")
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
